97bf6ae31bcf2fb4f2a74514623270576afd0ec5
brat <149518678+8r47fkin@users.noreply.github.com>
2023-11-15 19:28:21 +0530

    Update index.js
    a code to enter fullscreen, later to be changed as per players control design which is getting designed

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// full screen JAVASCRIPT
+  document.addEventListener(
+  "keydown",
+  (e) => {
+    if (e.key === "Enter") {
+      toggleFullScreen();
+    }
+  },
+  false,
+);
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
+
+// full screen scipt ends
+
